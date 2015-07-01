1bf843af62e3fc5debe6051f3ab7327380f3dd40
ladykate17 <stamm.katherine@gmail.com>
2015-07-01 12:07:28 -0600

    completed exercise with functional clock

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ $(document).on('ready', function() {
 	// Inner Shell 
 	var innerShell = $('<div class="inner-shell"></div>');
 
-	// Left AM/PM & Auto Labels 
-	var labels = $('<div class="label">CLOCKEX</div>');
+	// Brand Label
+	var brandlabel = $('<div class="brand label">CLOCKEX</div>');
 
 	// Clock Screen 
 	var clockScreen = $('<div class="screen"></div>');
@@ -19,54 +19,38 @@ $(document).on('ready', function() {
 	var am = $('<div class="clock-text am-pm">AM</div>');
 	var pm = $('<div class="clock-text am-pm">PM</div>');
 
+	// Radio Label 
+	var radiolabel = $('<div class="radio label">FM 88 92 96 100 104 108 MHz</div><div class="radio label">AM 54 60 80 100 130 160 x10kHz</div>');
+
 	
-	// //--- Clock workings - not functional ---//
-	// // hour
-	// var hour = function(){
-	// 	for (var i = 1; i <=12; i++){
-	// 	if ( i < 10 ){
-	// 		return '0'+ i;
-	// 	}
-	// 	else {
-	// 		return i;
-	// 	}
+	// //--- Clock workings ---//
 
-	// 	}
-	// }
-	// //minute
-	// var minute = function(){
-	// 	for (var j = 1; j < 60; j++){
-	// 	if ( j < 10 ){
-	// 		return '0'+j;
-	// 	} 
-	// 	else {
-	// 		return j;	
-	// 	}
-	// // $(pm).addClass('active');
+	var currentTime = new Date()
+	var hours = currentTime.getHours()
+	var minutes = currentTime.getMinutes()
+
+	if (minutes < 10)
+	minutes = "0" + minutes
+
+	var suffix = $(am).addClass('active');
 	
-	// 	}
-	// }
-
-	// for (var k = 1; k < 60; k++){
-	// 	hour()
-	// 	return;
-	// }
-
-	// // am-pm
-	$(pm).addClass('active');
-	// // $(am).addClass('active');
-
-
+	if (hours >= 12) {
+	suffix = $(pm).addClass('active'), $(am).removeClass('active');
+	hours = hours - 12;
+	}
+	if (hours == 0) {
+	hours = 12;
+	}
 
 
 	// clock face text
-	var text = $('<div class="clock-text time">'+ 12 + ':' + 45 +'</div>');
+	var text = $('<div class="clock-text time">' + hours + ":" + minutes + '</div>');
 
 
 	$('body').prepend(container)
 	$('.container').append(outerShell);
 	outerShell.append(innerShell);
-	innerShell.append(clockScreen).prepend(labels);
+	innerShell.append(clockScreen).prepend(brandlabel).append(radiolabel);
 	clockScreen.append(text).append(am).append(pm);
 
 
